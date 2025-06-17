13786ac21dab4d8f4544290461e81491cd9276e0
Moinuddin-dotcom <moinchy7@gmail.com>
2025-06-17 16:26:23 +0600

    mongodb connected - user saving on database - working on create post server side

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const app = express();
+const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const port = process.env.PORT || 8002
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.afwrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const database = client.db('roadmapDB')
+        const userCollection = database.collection('users')
+
+        // save all logged in user in the database
+        app.post('/users', async (req, res) => {
+            const userInfo = req.body
+            const query = { email: userInfo?.email }
+            const existingUser = await userCollection.findOne(query)
+            if (existingUser) return res.send({ message: "User already exists", insertedId: null })
+            const result = await userCollection.insertOne(userInfo)
+            res.send(result);
+        })
+
+        // get logged in user info from database
+        app.get('/users', async (req, res) => {
+            const user = await userCollection.find().toArray()
+            res.send(user);
+        })
+
+        // get user role by email
+        app.get('/users/role/:email', async (req, res) => {
+            const email = req.params.email
+            // console.log(email)
+            const query = { email }
+            const result = await userCollection.findOne(query)
+            // console.log(result)
+            res.send({ role: result?.role });
+        })
+
+        // Get Single User by email
+        app.get('/users/singleUser/:email', async (req, res) => {
+            const { email } = req.params
+            const result = await userCollection.findOne({ email })
+            res.send(result);
+        })
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Roadmap app Server....')
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
